9bfc315a643cb9c8bb5500f14700b8ded5d3203c
palmtrey <52005788+palmtrey@users.noreply.github.com>
2021-11-23 17:15:19 -0500

    Update main.py

--- main.py
+++ main.py
@@ -1,11 +1,23 @@
 def is_valid_walk(walk):
-    n_counter = 0
-    for x in walk:
-        if x == "n":
-            n_counter += 1
-        
+    loc = [0, 0]      # [x, y]
+    # walk = [dir, dir, dir, dir]
 
+    for dir in walk:
+        if dir == 'n':
+            loc[1] += 1
+        elif dir == 's':
+            loc[1] -= 1
+        elif dir == 'w':
+            loc[0] -= 1
+        elif dir == 'e':
+            loc[0] += 1
+        else:
+            print('You crazy')
 
+    if loc == [0,0] and len(walk) == 10:
+        return True
+    else:
+        return False
 
-walk = ["n", "s", "n", "s"]
-is_valid_walk()
+if __name__ == '__main__':
+    print(is_valid_walk(['n','s','n','s','n','s','n','s','n','s']))
